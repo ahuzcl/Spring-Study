35dca5c39728304306e42fb30a3c77d05d42b234
Cunliang_AHU <33220873+ahuzcl@users.noreply.github.com>
2020-10-20 15:39:22 +0800

    注解

---
+++ spring-05-Autowired/src/main/java/com/cunliang/pojo/Cat.java
@@ -0,0 +1,7 @@
+package com.cunliang.pojo;
+
+public class Cat {
+    public void spark(){
+        System.out.println("miao");
+    }
+}
---
+++ spring-05-Autowired/src/main/java/com/cunliang/pojo/Dog.java
@@ -0,0 +1,7 @@
+package com.cunliang.pojo;
+
+public class Dog {
+    public void spark(){
+        System.out.println("wang");
+    }
+}
